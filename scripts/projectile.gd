f553cbfcd0ab3205e26a82b47113b0c56f259724
Senatrius <g.makutenas@gmail.com>
2026-02-08 20:15:28 +0200

    add block destruction

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -12,3 +12,22 @@ func _ready() -> void:
 	
 func _process(delta: float) -> void:
 	position += velocity * delta
+
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body)
+	if body is TileMapLayer:
+		var direction = transform.x
+		var impact_pos = global_position + (direction * 2.0)
+		var local_pos = body.to_local(impact_pos)
+		var map_pos = body.local_to_map(local_pos)
+		
+		var tile_data = body.get_cell_tile_data(map_pos)
+		print(tile_data)
+		if tile_data:
+			var can_destroy = tile_data.get_custom_data("breakable")
+			
+			if can_destroy:
+				body.erase_cell(map_pos)
+		
+		queue_free()
